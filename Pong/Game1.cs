281a3e610dba8c7039e4a91e7798b2864583aac9
Wojciech-Klis <14KlisW@student.smchull.org>
2025-09-23 12:18:13 +0100

    idk whats wrong

--- Pong/Game1.cs
+++ Pong/Game1.cs
@@ -9,18 +9,18 @@ namespace Pong
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Texture2D _paddle1, _paddle2, _ball;
-        private int _paddle1XPos = 0;
-        private int _paddle1YPos = 0;
+        private int _paddle1XPos;
+        private int _paddle1YPos;
         private int _paddle1Width = 50;
-        private int _paddle1Height = 0;
-        private int _paddle2XPos = 0;
-        private int _paddle2YPos = 0;
-        private int _paddle2Width = 0;
-        private int _paddle2Height = 0;
-        private int _ballXPos = 0;
-        private int _ballYPos = 0;
-        private int _ballWidth = 0;
-        private int _ballHeight = 0;
+        private int _paddle1Height = 300;
+        private int _paddle2XPos;
+        private int _paddle2YPos;
+        private int _paddle2Width = 50;
+        private int _paddle2Height = 300;
+        private int _ballXPos;
+        private int _ballYPos;
+        private int _ballWidth = 100;
+        private int _ballHeight = 100;
         private Rectangle _paddle1Rectangle, _paddle2Rectangle, _ballRectangle;
 
         public Game1()
@@ -32,9 +32,11 @@ namespace Pong
             IsMouseVisible = true;
 
             _paddle1XPos = 50;
-            _paddle1YPos = _graphics.PreferredBackBufferHeight / 2 + _paddle1Height / 2;
-            _paddle2XPos = _graphics.PreferredBackBufferWidth - 50;   
-            _paddle1YPos = _graphics.PreferredBackBufferHeight / 2 + _paddle2Height / 2;
+            _paddle1YPos = (_graphics.PreferredBackBufferHeight / 2);
+            _paddle2XPos = (_graphics.PreferredBackBufferWidth - 50);   
+            _paddle2YPos = (_graphics.PreferredBackBufferHeight / 2);
+            _ballXPos = (_graphics.PreferredBackBufferWidth / 2);
+            _ballYPos = (_graphics.PreferredBackBufferHeight / 2);
         }
 
         protected override void Initialize()
@@ -48,9 +50,9 @@ namespace Pong
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            Content.Load<Texture2D>("ball");
-            Content.Load<Texture2D>("paddle1");
-            Content.Load<Texture2D>("paddle2");
+            _paddle1 = Content.Load<Texture2D>("ball");
+            _paddle2 = Content.Load<Texture2D>("paddle1");
+            _ball = Content.Load<Texture2D>("paddle2");
 
             _paddle1Rectangle = new Rectangle(_paddle1XPos, _paddle1YPos, _paddle1Width, _paddle1Height);
             _paddle2Rectangle = new Rectangle(_paddle1XPos, _paddle2YPos, _paddle2Width, _paddle2Height);
@@ -80,9 +82,11 @@ namespace Pong
         {
             GraphicsDevice.Clear(Color.Black);
 
-            _spriteBatch.Draw(_paddle1, _paddle1Rectangle, Color.White);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_paddle1, _paddle1Rectangle, Color.Whit);
             _spriteBatch.Draw(_paddle2, _paddle2Rectangle, Color.White);
             _spriteBatch.Draw(_ball, _ballRectangle, Color.White);
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
